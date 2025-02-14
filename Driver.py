a985ffd5179be75c98ada8e18ceaccc44ce432ad
Killian <killian2100@hotmail.com>
2025-02-14 16:38:18 +0000

    DQN Stats 1.3
    Average-score, duration (steps), and loss stats added, episode video recording also implemented.

--- Driver.py
+++ Driver.py
@@ -1,23 +1,20 @@
-# %%
 import torch
-import matplotlib
-import matplotlib.pyplot as plt
-from time import sleep, time
+from itertools import count
 from vizdoom import gymnasium_wrapper
+from matplotlib import pyplot as plt
+from IPython.display import clear_output
 import gymnasium
 import numpy as np
 import random
 import math
 
+import rlqlearning.rlqlUtil as util
+import rlqlearning.models as models
+
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print(f"Running on device: {device}")
 env = gymnasium.make("VizdoomBasic-v0")#, render_mode="human")
 
-# %%
-import rlqlearning.rlqlUtil as util
-import rlqlearning.models as models
-
-# %%
 BATCH_SIZE = 128 # Transitions sampled from replay buffer
 GAMMA = 0.99 # Discount factor
 EPSILON_START = 0.9 # Start, end, and rate of decay of the epsilon value 
@@ -30,11 +27,8 @@ state, info = env.reset()
 screen_w = len(state['screen'])
 screen_h = len(state['screen'][0])
 channels =  3
-#print(screen_w)
-#print(screen_h)
 screen_state = torch.tensor(np.array(state['screen']), dtype=torch.float32, device=device).unsqueeze(0)
 
-# %%
 policy = models.DQN(screen_w, screen_h, channels, num_actions).to(device)
 target = models.DQN(screen_w, screen_h, channels, num_actions).to(device)
 target.load_state_dict(policy.state_dict())
@@ -42,9 +36,12 @@ target.load_state_dict(policy.state_dict())
 optimiser = torch.optim.AdamW(policy.parameters(), lr=LEARNING_RATE, amsgrad=True)
 memory = util.ReplayMemory(10000)
 
-# %%
 steps_done = 0
 episode_durations = []
+episode_scores = []
+avg_scores = []
+running_avg_scores =  []
+loss_aggr = []
 
 def select_action(observation):
     global steps_done
@@ -54,37 +51,77 @@ def select_action(observation):
     steps_done += 1
     if sample > eps_threshold:
         with torch.no_grad():
-            return policy(observation).max(1).indices.view(1, 1)
+            return policy(observation.to(device)).max(1).indices.view(1, 1)
     else:
         return torch.tensor([[env.action_space.sample()]], device=device, dtype=torch.long)
 
-# %%
 if torch.cuda.is_available():
-    num_episodes = 1000
+    num_episodes = 500
 else:
-    num_episodes = 50
+    num_episodes = 5
 
 for i in range(num_episodes):
+    if i == num_actions - 1:
+        tmp_env = gymnasium.make("VizdoomBasic-v0", render_mode="rgb_array")
+        env = gymnasium.wrappers.RecordVideo(env=tmp_env, video_folder="./rlqlearning/Recordings", name_prefix="test-video", episode_trigger=lambda t: t % 50 == 0)
     done = False
     state, info = env.reset()
     state = torch.tensor(np.array(state['screen']), dtype=torch.float32, device=device).unsqueeze(0)
     state = torch.transpose(state, 1, 3)
-    #print(state.shape)
     score = 0
-    while not done:
+    for t in count():
         action = select_action(state)
-        #print(action.item())
-        observation, reward, done, trunc, info = env.step(action.item())
+        observation, reward, terminated, truncated, info = env.step(action.item())
         score += reward
-        if done:
+        done = terminated or truncated
+        if terminated:
             next_state = None
-            print(f"Episode {i} Score: {score}")
         else:
             next_state = torch.tensor(observation['screen'], dtype=torch.float32, device=device).unsqueeze(0)
             next_state = torch.transpose(next_state, 1, 3)
 
         # Store the transition in memory
+        #print(dir(memory))
         memory.push(state, action, next_state, reward)
 
         # Move to the next state
         state = next_state
+        
+        loss = util.optimize_model(memory, policy, target, optimiser, BATCH_SIZE, GAMMA, device)
+        # Soft update of the target network's weights
+        # θ′ ← τ θ + (1 −τ )θ′
+        target_state_dict = target.state_dict()
+        policy_state_dict = policy.state_dict()
+        for key in policy_state_dict:
+            target_state_dict[key] = policy_state_dict[key]*TAU + target_state_dict[key]*(1-TAU)
+        target.load_state_dict(target_state_dict)
+        
+        if done:
+            if loss != None:
+                loss_aggr.append(loss)
+            episode_durations.append(t + 1)
+            episode_scores.append(score)
+            avg_scores.append((sum(episode_scores)) / (i+1))
+
+            clear_output(wait=True)
+
+            print(f"Episode {i} Score: {score}, Average: { (sum(episode_scores))  / (i+1) }")
+            util.plot_durations(episode_durations)
+            util.plot_avg_scores(avg_scores)
+            if i >= 3:
+                running_avg_scores.append((sum(avg_scores[-3:]))  / 3)
+                #print(f"Episode {i} Score: {score}, Running Average (3 Episodes): {running_avg_scores[-1]}")
+                util.plot_running_avg_scores(running_avg_scores)
+            util.plot_loss(loss_aggr)
+            break
+clear_output(wait=True)
+print('Complete')
+util.plot_durations(episode_durations, show_result=True)
+util.plot_avg_scores(avg_scores, show_result=True)
+util.plot_running_avg_scores(running_avg_scores, show_result=True)
+util.plot_loss(loss_aggr, show_result=True)
+plt.ioff()
+plt.show()
+env.close()
+
+
