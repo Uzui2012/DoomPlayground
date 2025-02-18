1d261e40c2e6dd7e1ecf709939f801103da9cfc6
Killian <killian2100@hotmail.com>
2025-02-18 22:05:29 +0000

    Preparing hyperparameter tuning 1.0

--- rlqlearning/models.py
+++ rlqlearning/models.py
@@ -6,20 +6,21 @@ import torch.nn.functional as F
 
 class DQN(nn.Module):
 
-    def __init__(self, w, h, c, num_actions):
+    def __init__(self, w, h, c, num_actions, conv_size_1 = 16, conv_size_2 = 32, conv_size_3 = 32, lin_size_1 = 512, lin_size_2 = 256):
         super(DQN, self).__init__()
-        self.conv1 = nn.Conv2d(c, 16, kernel_size=3, stride=2)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=2)
-        self.conv3 = nn.Conv2d(32, 32, kernel_size=3, stride=2)
+        self.conv1 = nn.Conv2d(c, conv_size_1, kernel_size=3, stride=2) #16 32 32
+        self.conv2 = nn.Conv2d(conv_size_1, conv_size_2, kernel_size=3, stride=2)
+        self.conv3 = nn.Conv2d(conv_size_2, conv_size_3, kernel_size=3, stride=2)
         
         def conv2d_size_out(size, kernel_size=3, stride=2):
             return (size - (kernel_size - 1) - 1) // stride  + 1
         convw = conv2d_size_out(conv2d_size_out(conv2d_size_out(w)))
         convh = conv2d_size_out(conv2d_size_out(conv2d_size_out(h)))
-        linear_input_size = convw * convh * 32
-        self.fc1 = nn.Linear(linear_input_size, 512)
-        self.fc2 = nn.Linear(512, 256)
-        self.fc3 = nn.Linear(256, num_actions)
+        linear_input_size = convw * convh * conv_size_3
+        
+        self.fc1 = nn.Linear(linear_input_size, lin_size_1) #512 256
+        self.fc2 = nn.Linear(lin_size_1, lin_size_2)
+        self.fc3 = nn.Linear(lin_size_2, num_actions)
 
     def forward(self, x):
         x = F.leaky_relu(self.conv1(x))
